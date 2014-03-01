1f34cc16ab2e82282ab7a4534b064f9c1a0c8fa2
Minions <minionshack@gmail.com>
2014-03-01 13:52:45 -0500

    edit routes.rb

--- minions/config/routes.rb
+++ minions/config/routes.rb
@@ -1,6 +1,7 @@
 Minions::Application.routes.draw do
-  devise_for :users
+ devise_for :users
  root "pages#home"
+ get "about" => "pages#about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
