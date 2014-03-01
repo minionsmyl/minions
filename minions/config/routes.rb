31bf8bcceca540f752cd64a1097c654eb784c666
Minions <minionshack@gmail.com>
2014-03-01 11:33:07 -0500

    create login and signup

--- minions/config/routes.rb
+++ minions/config/routes.rb
@@ -1,4 +1,5 @@
 Minions::Application.routes.draw do
+  devise_for :users
  root "pages#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
