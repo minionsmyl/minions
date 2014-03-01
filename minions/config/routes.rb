3743d7f7d48130bf7b37039869f20acd0c8e5f34
Minions <minionshack@gmail.com>
2014-03-01 10:11:09 -0500

    route homepage

--- minions/config/routes.rb
+++ minions/config/routes.rb
@@ -1,4 +1,5 @@
 Minions::Application.routes.draw do
+ root "pages#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
