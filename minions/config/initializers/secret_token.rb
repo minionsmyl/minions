1284c25fd31cc795b85fa984711a02653583ce2a
Minions <minionshack@gmail.com>
2014-03-01 09:57:52 -0500

    add file

---
+++ minions/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Minions::Application.config.secret_key_base = 'ee7c6b217695fa3ecf2009845a86cd8fdb7af2f334233b982e15cae30575ecca76a8c451ac261c7b568ad6767402f6041a9496b7385177a95743a4e2f69cd14f'
---
+++ minions/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Minions::Application.config.session_store :cookie_store, key: '_minions_session'
